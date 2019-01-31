7e03ce859bf9e229f47f55a76c42f2ecb3c5c822
ceraud <clement.eraud@capgemini.com>
2019-01-31 16:55:47 +0100

    :sparkles: add cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import dbConnection from "./dbConnection";
 import * as express from "express";
 import * as graphqlHTTP from "express-graphql";
 import { todoSchema, todoRoot } from "./models/TodoModel";
+import * as cors from "cors";
 
 dbConnection();
 
 const app = express();
+app.use(cors());
 
 app.use(
   "/todo",
